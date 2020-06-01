58419211d4399a00f43e92fc980bd3de95e71686
Dhaval Dashadia <dashadiadhaval313@gmail.com>
2020-06-01 16:29:09 +0530

    username in navv bar

--- project2/static/js/home.js
+++ project2/static/js/home.js
@@ -1,1 +1,20 @@
 document.addEventListener('DOMContentLoaded', usrnm());
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.usr').innerHTML = localStorage.getItem('name');
+});
+function change_username() {
+    var i = 0;
+    while (i === 0){
+        var name = prompt("Change your name: ", localStorage.getItem('name'));
+        if (name.length > 12) {
+            name = prompt("Length must be less thane 12:", localStorage.getItem('name'));
+        }
+        if (name != null && name != "") {
+            localStorage.setItem('name', name);
+            i = 1;
+        }
+        else if (name == null) {
+            i = 1;
+        }
+    }
+}
